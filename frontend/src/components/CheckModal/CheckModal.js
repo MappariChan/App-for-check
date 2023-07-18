0248b9c462fe1cdff48dbb2cab1b205b030ff516
Mafusaillo <danylo.prots.kn.2021@lpnu.ua>
2023-07-18 11:24:51 +0300

    Small fix

--- frontend/src/components/CheckModal/CheckModal.js
+++ frontend/src/components/CheckModal/CheckModal.js
@@ -1,7 +1,10 @@
 import classes from "./CheckModal.module.css";
 
-const CheckModal = ({ close }) => {
-  return <div onClick={close} className={classes.modal}></div>;
+const CheckModal = ({ close, checkTitle, checkNumber }) => {
+  return <div onClick={close} className={classes.modal}>
+    <h2>{checkTitle}</h2>
+    <p>{checkNumber}</p>
+  </div>;
 };
 
 export default CheckModal;
